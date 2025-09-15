7f43459cfed4f256dc8d35fadfb24fa55102b16d
revmeetoo <revmeetoo@outlook.com>
2025-09-16 02:17:15 +0300

    Update helper.js

--- www/helper.js
+++ www/helper.js
@@ -1,7 +1,5 @@
-cordova.define("cordova-plugin-helper.helper", function(require, exports, module) {
-    var exec = require("cordova/exec");
-    var helper = function () {this.name = "helper";};
-    helper.prototype.func01 = function (successCallback, failureCallback) {
-    exec(successCallback, failureCallback, "helper", "func01", []);};
-    module.exports = new helper();
-});
+var exec = require("cordova/exec");
+var helper = function () {this.name = "helper";};
+helper.prototype.func01 = function (successCallback, failureCallback) {
+exec(successCallback, failureCallback, "helper", "func01", []);};
+module.exports = new helper();
